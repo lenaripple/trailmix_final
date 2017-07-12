e497e0dc823f22efc18e2550383c129d53833765
Lena Ripple <ripplelena@gmail.com>
2017-07-12 11:02:51 -0600

    passwords hashing and ready to decouple

--- backend/src/modules/users/user.routes.js
+++ backend/src/modules/users/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import validate from 'express-validation';
+import { authLocal } from '../../services/auth.services';
 import * as userController from './user.controller';
 import userValidation from './user.validation';
 
 const routes = new Router();
 
 routes.post('/signup', validate(userValidation.signup), userController.signUp);
+routes.post('/login', authLocal, userController.login);
 
 export default routes;
